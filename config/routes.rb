4004b7232e393eac163e07d8d71ec1c9958a3395
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-17 21:50:04 -0700

    Generate app controller with index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
+  get "/app", to: "app#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
